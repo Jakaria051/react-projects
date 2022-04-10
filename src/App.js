65b69dfba0cfdb1a681f2469804981de1c91d065
jacjakaria051 <jacjakaria51@gmail.com>
2022-04-10 19:37:42 +0600

    crud

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
-import { useState } from "react";
-import Home from "./components/Home.js";
-import Navbar from "./components/Navbar.js";
+import "bootstrap/dist/css/bootstrap.css";
+import * as React from "react";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import Row from "react-bootstrap/Row";
+import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
+import CreateProduct from "./components/product/CreateProduct";
+import EditProduct from "./components/product/EditUser";
+import ProductList from "./components/product/ProductList";
+
 function App() {
-  const [darkMode, setDarkMode] = useState(true);
-
-  function togglerMode() {
-    setDarkMode((prevState) => !prevState);
-  }
-
   return (
-    <div className="container">
-      <Navbar darkMode={darkMode} togglerMode={togglerMode} />
-      <Home darkMode={darkMode} />
-    </div>
+    <Router>
+      <Navbar bg="primary">
+        <Container>
+          <Link to={"/"} className="navbar-brand text-white">
+            Basic Crud App
+          </Link>
+        </Container>
+      </Navbar>
+
+      <Container className="mt-5">
+        <Row>
+          <Col md={12}>
+            <Routes>
+              <Route path="/product/create" element={<CreateProduct />} />
+              <Route path="/product/edit/:id" element={<EditProduct />} />
+              <Route exact path="/" element={<ProductList />} />
+            </Routes>
+          </Col>
+        </Row>
+      </Container>
+    </Router>
   );
 }
 
