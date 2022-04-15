b463a7e23cc5007263dd4bdeb8a899492a4332a6
jacjakaria051 <jacjakaria51@gmail.com>
2022-04-15 15:39:56 +0600

    used redux for state management

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
-
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import ProductDetail from "./components/ProductDetail";
+import ProductListing from "./components/ProductListing";
 function App() {
   return (
-    <>
-      <h1>fdfdf</h1>
-    </>
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<ProductListing />} />
+        <Route path="/product/:productId" element={<ProductDetail />} />
+        <Route>404 Not Found</Route>
+      </Routes>
+    </div>
   );
 }
 
