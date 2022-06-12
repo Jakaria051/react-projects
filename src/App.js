ec094ce49b995fb5ffd79637ea4c52aeae976ed6
jacjakaria051 <jacjakaria51@gmail.com>
2022-06-12 12:43:29 +0600

    tour project completed

--- src/App.js
+++ src/App.js
@@ -1,21 +1,57 @@
-import React, { useState } from "react";
-import List from "./List";
-import data from "./data";
+import React, { useEffect, useState } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
+const url = "https://course-api.com/react-tours-project";
 
 function App() {
-  const [people, setPeople] = useState(data);
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+
+  const fetchTours = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>no tour left</h2>
+          <button className="btn" onClick={() => fetchTours()}>
+            refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
   return (
     <main>
-      <section className="container">
-        <h3>{people.length} birthday today</h3>
-        <List people={people} />
-        {people.length > 0 && (
-          <button onClick={() => setPeople([])}>Clear all</button>
-        )}
-        {people.length === 0 && (
-          <button onClick={() => setPeople(data)}>Get all</button>
-        )}
-      </section>
+      <Tours tours={tours} removeTour={removeTour} />
     </main>
   );
 }
